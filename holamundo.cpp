af0e78f093c052b44729d1b20d3798379c56c2ca
Rodrigo Corrales Rueda <rodrigocor.alumn@cescristorey.com>
2020-11-06 14:39:22 +0100

    cambio merge

---
+++ holamundo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int hola(int hola) {
+
+	cout << “Hola mundo” << endl;		
+	return hola;
+}
+
+int main() {
+
+    string hola = NULL;
+    int holaa = 9;
+	
+    holaa= hola(holaa);
+
+    return 0;
+}
